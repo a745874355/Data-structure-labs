503c8278f7ae168318383371961c92fdd11ec774
shichao <swang206@myseneca.ca>
2020-01-14 00:12:55 -0500

    factorial finished

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -2,13 +2,17 @@
 /*                                                */
 /*  Lab 1 Starter file                            */
 /*                                                */
-/*  Name:                                         */
-/*  Student number:                               */
+/*  Name: Shichao Wang                            */
+/*  Student number: 106194152                     */
 /*                                                */
 /**************************************************/
 
 unsigned int factorial (unsigned int n){
-
+    unsigned int result = 1;
+    for(; n > 0; n--){
+        result *= n;
+    }
+    return result;
 }
 
 double power (double base, unsigned int n){
