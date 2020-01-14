2ec7cb66ce5c61aa55cdb36a8c1e47560cba51ee
shichao <swang206@myseneca.ca>
2020-01-14 00:51:06 -0500

    lab1 code finished

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -7,22 +7,29 @@
 /*                                                */
 /**************************************************/
 
-unsigned int factorial (unsigned int n){
-    unsigned int result = 1;
-    for(; n > 0; n--){
-        result *= n;
+unsigned int factorial (unsigned int n){ //Worst complex: O(n) = 2+4n
+    unsigned int result = 1; // 1
+    for(; n > 0; n--){ // 2n
+        result *= n; // 2n
     }
-    return result;
+    return result; // 1
 }
 
-double power (double base, unsigned int n){
-    double result = base;
-    if(n == 0) return 1;
-    for(unsigned int i = n - 1; i > 0; i--){
-        result *= base;
+double power (double base, unsigned int n){ //Worst complex: O(n) = 4n
+    if(n == 0) return 1; //1
+    double result = base; //1
+    for(unsigned int i = n - 1; i > 0; i--){ // 1 + 2(n-1) = 1+2n-2 = 2n-1
+        result *= base; //2n - 1
     }
-    return result;
+    return result; //1
 }
 unsigned int fibonacci (unsigned int n){
-	
+	if(n == 0) return 0; // 1
+    if(n == 1) return 1; // 1
+    unsigned int result = 1; // 1
+    n-=2; // 2
+    for(; n >0;n--){ // 2
+        result += fibonacci(n); 
+    }
+    return result; // 1 
 }
